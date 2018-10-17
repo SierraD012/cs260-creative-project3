ad35fb154da6e2bee14999ab272ab035065a9279
Sierra Davis <sd.birdlover@gmail.com>
2018-10-17 16:51:14 +0000

    setting up some angular stuff

---
+++ scripts.js
@@ -0,0 +1,33 @@
+angular.module('mainActivityList', [])
+.controller('mainController', mainCtrl)
+//.directive('activity', activityDirective);  //maybe use this to populate the list of activities once category is selected 
+  
+function mainCtrl ($scope) {
+    
+  $scope.activities = [];
+  
+  $scope.addNewActivity = function (activity) {
+      console.log(">AddNewActivity: called")
+      //put this in a factory
+        $scope.activities.push({ 
+            title: activity.title,
+            category: activity.category,    //enum or dropdown
+            description: activity.description,
+            imgURL: activity.imgURL
+        });
+    
+    console.log(">ActivitiesList length= " + $scope.activities.length);
+  };
+  
+  //Use this init function to populate the activityList with our hard-coded activities
+  var init = function () {
+        $scope.activities.push({ 
+            title: "Sample Activity 1",
+            category: "category",    //enum?
+            description: "description",
+            imgURL: "imgURL"
+        });
+     };
+  init(); //pre-populate list right after page loads
+} 
+//end mainController ---------
