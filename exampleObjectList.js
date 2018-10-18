508f0bb1c3a4bb9f855a09ba90809e787c6ef6e9
Mitchell Mortensen <mitchneutron@gmail.com>
2018-10-18 21:00:28 +0000

    Added an example object list that we could use. Added notes to index.html

--- exampleObjectList.js
+++ exampleObjectList.js
@@ -1,17 +1,17 @@
 {
-    "objects": [
-    {
-        "imageUrl": "someBrokenUrl.com/somedumbimage",
-        "description": "This is a description of the activity",
-        "category": "Painting",
-        "name": "Finger painting at HBLL"
-    },
+    "objects": [{
+            "imageUrl": "someBrokenUrl.com/somedumbimage",
+            "description": "This is a description of the activity",
+            "category": "Painting",
+            "name": "Finger painting at HBLL"
+        },
+
         {
-        "imageUrl": "someBrokenUrl.com/secondImage",
-        "description": "This is a description of the second activity",
-        "category": "Skiing",
-        "name": "Go skiing at sundance!!"
-    }
+            "imageUrl": "someBrokenUrl.com/secondImage",
+            "description": "This is a description of the second activity",
+            "category": "Skiing",
+            "name": "Go skiing at sundance!!"
+        }
 
     ]
 }
